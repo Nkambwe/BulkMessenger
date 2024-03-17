680bbe15ccd90815fd3655227573998133d70067
Nkambwe Mark <35249251+Nkambwe@users.noreply.github.com>
2024-03-17 08:26:43 +0300

    Update ConfigParam.cs

--- BulkMessager/Settings/ConfigParam.cs
+++ BulkMessager/Settings/ConfigParam.cs
@@ -3,13 +3,13 @@
         /// <summary>
         /// Gets the name of an environment variable with connection string settings
         /// </summary>
-        public static string ConnectionString => "MESSENGER_CONNECTIONSTRING";
+        public static string ConnectionString => "YOUR_CON_ENV_VAR_NAME";
 
         /// <summary>
         /// Gets the name of an environment variable for SMS messaging server url
         /// </summary>
-        public static string MessageServerUrl => "MESSENGER_SMSSERVERURL";
+        public static string MessageServerUrl => "YOUR_MESSAGE_CLIENT_URL_ENV_VAR_NAME";
 
-        public const string APIKEY = "_l0k1@Farz3gard";
+        public const string APIKEY = "YOUR_ENCRPTION_KEY";
     }
 }
